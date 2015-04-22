a0e15cdd2075591d85ec02c6dd64571a88355f29
Kim Kiogora <kimkiogora@gmail.com>
2015-04-22 12:40:49 +0300

    Create UptimeClient.cpp

---
+++ src/UptimeClient.cpp
@@ -0,0 +1,15 @@
+/**
+   @Usage: Uptime Client
+   @uthor: kim kiogora	<kimkiogora@gmail.com>
+*/
+
+#include "UptimeClient.h"
+
+/**
+ * Main Entry Zone.
+*/
+int main() {
+	Uptime ut;
+	ut.init();
+	return EXIT_SUCCESS;
+}
